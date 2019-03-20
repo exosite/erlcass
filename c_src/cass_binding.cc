4d755dd1e877118593f38f2df2715b639c84110a
Jesper Lundgren <jesperlundgren@exosite.com>
2019-03-20 15:32:03 +0800

    implicit conversion from integer to double

--- c_src/cass_binding.cc
+++ c_src/cass_binding.cc
@@ -217,14 +217,24 @@ template <typename T> ERL_NIF_TERM cass_set_from_nif(ErlNifEnv* env, T obj, size
         case CASS_VALUE_TYPE_FLOAT:
         case CASS_VALUE_TYPE_DOUBLE:
         {
-            double val_double;
-            if(!enif_get_double(env, value, &val_double))
-                return make_badarg(env);
+            double val_double = 0;
+            long val_long = 0;
+            bool success = false;
+            if(enif_get_double(env, value, &val_double)) {
+                success = true;
+            } else if (enif_get_int64(env, value, &val_long )) {
+                val_double = static_cast<double>(val_long);
+                success = true;
+            }
 
-            if(data_type->value_type() == CASS_VALUE_TYPE_FLOAT)
-                return cass_error_to_nif_term(env, fun.set_float(obj, index, static_cast<float>(val_double)));
-            else
-                return cass_error_to_nif_term(env, fun.set_double(obj, index, val_double));
+            if(success) {
+                if(data_type->value_type() == CASS_VALUE_TYPE_FLOAT)
+                    return cass_error_to_nif_term(env, fun.set_float(obj, index, static_cast<float>(val_double)));
+                else
+                    return cass_error_to_nif_term(env, fun.set_double(obj, index, val_double));
+            }
+
+            return make_badarg(env);
         }
 
         case CASS_VALUE_TYPE_INET:
